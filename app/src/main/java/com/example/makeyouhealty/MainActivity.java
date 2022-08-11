f19cea6ca54050e4fa8e42a9e644e43f18a42eeb
kanharajput91 <kanharajput91@gmail.com>
2022-08-11 19:37:17 +0530

    initial-commit

---
+++ app/src/main/java/com/example/makeyouhealty/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.makeyouhealty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
